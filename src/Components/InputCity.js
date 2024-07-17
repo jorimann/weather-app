d5409ab61623495e3be13b7a50e7ed7f4e2e429a
Yevhen Kravchenko <jorimann@gmail.com>
2024-07-17 11:29:36 +0200

    initial commit

---
+++ src/Components/InputCity.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const InputCity = ({ onSubmitHandler, city, onInputHandler }) => {
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={city}
+        onChange={onInputHandler}
+        placeholder="City..."
+      />
+      <br />
+      <button className="input_btn" type="submit" onClick={onSubmitHandler}> 
+       {/* <button class="btn btn-primary"  type="submit" onClick={onSubmitHandler}>  */}
+
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default InputCity;
